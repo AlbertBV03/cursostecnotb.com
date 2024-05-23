ec751e14b759312ba650bdf81f560156d880e2a1
AngelEstrada21 <angel.et.tc12@gmail.com>
2024-05-22 23:21:41 -0600

    actualizacion del las vistas de los manuales

---
+++ views/manual/viewmanuales.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+use yii\helpers\Markdown;
+
+/** @var yii\web\View $this */
+/** @var app\models\Manual $model */
+
+$this->params['breadcrumbs'][] = ['label' => 'Manuals', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="container-fluid">
+<div class="manual-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div>
+        <?= Markdown::process($model->nombre) ?>
+    </div>
+    <div>
+        <?= Markdown::process($model->descripcion) ?>
+    </div>
+    <div>
+        <?= Markdown::process($model->requisitos) ?>
+    </div>
+    <div>
+        <?= Markdown::process($model->objetivo) ?>
+    </div>
+    <div>
+        <?= Html::img(Url::to('@web/' . $model->imagen)) ?>
+    </div>
+
+
+</div>
+</div>
